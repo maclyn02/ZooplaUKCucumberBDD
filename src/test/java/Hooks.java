99a627b20475bcb9d363541b078b3c761cfad467
maclyn02 <mac.afonso@yahoo.com>
2020-01-20 20:09:01 +0000

    updates

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -4,6 +4,7 @@ import cucumber.api.java.Before;
 
 public class Hooks {
 
+    //Global hooks
     @Before
     public void start(){
         BrowserFactory.initBrowser();
@@ -13,4 +14,11 @@ public class Hooks {
     public void tearDown(){
         BrowserFactory.close();
     }
+
+    //@Before(order=num) to specify order for global hooks
+
+    //Custom hooks
+    @Before("@Search")public void printSearch(){ System.out.println("Starting Search");  }
+
+    @After("@Search")public void endSearch(){ System.out.println("Completed Search");  }
 }
